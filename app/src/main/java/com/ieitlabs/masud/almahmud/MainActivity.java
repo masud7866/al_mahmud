4c1f0568792ca9b7638fbd0a163e4bc1d4232ca7
Masud Rahman <masud7866@gmail.com>
2018-10-10 20:01:46 +0600

    Mass Change in design

---
+++ app/src/main/java/com/ieitlabs/masud/almahmud/MainActivity.java
@@ -0,0 +1,28 @@
+package com.ieitlabs.masud.almahmud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("আল মাহমুদ");
+
+        Button exit = (Button) findViewById(R.id.exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+}
--- app/src/main/java/com/ieitlabs/masud/almahmud/SplashActivity.java
+++ app/src/main/java/com/ieitlabs/masud/almahmud/SplashActivity.java
@@ -1,13 +1,51 @@
 package com.ieitlabs.masud.almahmud;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class SplashActivity extends AppCompatActivity {
+
+    // used to know if the back button was pressed in the splash screen activity and avoid opening the next activity
+    private boolean mIsBackButtonPressed;
+    private static final int SPLASH_DURATION = 3000; // 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+
+        // run a thread after 2 seconds to start the home screen
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                // make sure we close the splash screen so the user won't come back when it presses back key
+
+                finish();
+
+                if (!mIsBackButtonPressed) {
+                    // start the home screen if the back button wasn't pressed already
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    SplashActivity.this.startActivity(intent);
+                }
+
+            }
+
+        }, SPLASH_DURATION); // time in milliseconds (1 second = 1000 milliseconds) until the run() method will be called
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        // set the flag to true so the next activity won't start up
+        mIsBackButtonPressed = true;
+        super.onBackPressed();
     }
 }
