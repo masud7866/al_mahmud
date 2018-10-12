644bd6d5c0c4fa96ba25a41f2f0ef18740e82480
Masud Rahman <masud7866@gmail.com>
2018-10-12 15:04:21 +0600

    Character encoding fixed

--- app/src/main/java/com/ieitlabs/masud/almahmud/AboutActivity.java
+++ app/src/main/java/com/ieitlabs/masud/almahmud/AboutActivity.java
@@ -2,6 +2,7 @@ package com.ieitlabs.masud.almahmud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class AboutActivity extends AppCompatActivity {
@@ -14,6 +15,8 @@ public class AboutActivity extends AppCompatActivity {
         setTitle("আমাদের কথা");
 
         WebView webView = findViewById(R.id.about);
+        WebSettings settings = webView.getSettings();
+        settings.setDefaultTextEncodingName("utf-8");
         webView.loadUrl("file:///android_asset/about_text.html");
     }
 
